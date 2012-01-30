d0125292ca5e4befbdebf7d8fe60f720528a1253
Jerome Etienne <jerome.etienne@gmail.com>
2012-01-30 10:01:33 +0100

    js/tquery.scene.js added message if webgl is required but not available

--- js/tquery.scene.js
+++ js/tquery.scene.js
@@ -19,18 +19,15 @@ tQuery.Scene	= function()
 	this._scene	= new THREE.Scene();
 
 	// create a renderer
-	if( Detector.webgl ){
+	if( this._hasWebGL ){
 		this._renderer = new THREE.WebGLRenderer({
 			antialias		: true,	// to get smoother output
 			preserveDrawingBuffer	: true	// to allow screenshot
 		});
 		this._renderer.setClearColorHex( 0xBBBBBB, 1 );
-	// uncomment if webgl is required
-	//}else{
-	//	Detector.addGetWebGLMessage();
-	//	return true;
 	}else{
-		this._renderer	= new THREE.CanvasRenderer();
+		this._addGetWebGLMessage();
+		throw new Error("WebGL required and not available")
 	}
 	// FIXME this window dimension is crap
 	this._renderer.setSize( window.innerWidth, window.innerHeight );
@@ -48,13 +45,60 @@ tQuery.pluginsMixin(tQuery.Scene);
 
 tQuery.Scene.prototype.destroy	= function()
 {
-	
 }
 
 //////////////////////////////////////////////////////////////////////////////////
-//										//
+//		WebGL Support							//
 //////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * true if webgl is available, false otherwise
+*/
+tQuery.Scene.prototype._hasWebGL	= (function(){
+	// test from Detector.js
+	try{
+		return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' );
+	} catch( e ){
+		return false;
+	}
+})();
+
+/**
+*/
+tQuery.Scene.prototype._addGetWebGLMessage	= function(parent)
+{
+	parent	= parent || document.body;
+	
+	// message directly taken from Detector.js
+	var domElement = document.createElement( 'div' );
+	domElement.style.fontFamily = 'monospace';
+	domElement.style.fontSize = '13px';
+	domElement.style.textAlign = 'center';
+	domElement.style.background = '#eee';
+	domElement.style.color = '#000';
+	domElement.style.padding = '1em';
+	domElement.style.width = '475px';
+	domElement.style.margin = '5em auto 0';
+	domElement.innerHTML = window.WebGLRenderingContext ? [
+		'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br />',
+		'Find out how to get it <a href="http://get.webgl.org/">here</a>.'
+	].join( '\n' ) : [
+		'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br/>',
+		'Find out how to get it <a href="http://get.webgl.org/">here</a>.'
+	].join( '\n' );
+
+	parent.appendChild(domElement);
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//		add/remove object3D						//
+//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * add an object to the scene
+ * 
+ * @param {tQuery.Object3D} object3D to add to the scene (THREE.Object3D is accepted)
+*/
 tQuery.Scene.prototype.add	= function(object3d)
 {
 	if( object3d instanceof tQuery.Object3D ){
@@ -63,20 +107,34 @@ tQuery.Scene.prototype.add	= function(object3d)
 		}.bind(this));
 	}else if( object3d instanceof THREE.Object3D ){
 		this._scene.add(object3d)		
-	}else	console.assert(false);
+	}else	console.assert(false, "invalid type");
 	// for chained API
 	return this;
 }
+
+/**
+ * remove an object to the scene
+ * 
+ * @param {tQuery.Object3D} object3D to add to the scene (THREE.Object3D is accepted)
+*/
 tQuery.Scene.prototype.remove	= function(object3d)
 {
-	console.assert(object3d instanceof THREE.Object3D)
-	this._scene.remove(object3d)
+	if( object3d instanceof tQuery.Object3D ){
+		object3d.each(function(object3d){
+			this._scene.remove(object3d)
+		}.bind(this));
+	}else if( object3d instanceof THREE.Object3D ){
+		this._scene.remove(object3d)
+	}else	console.assert(false, "invalid type");
 	// for chained API
 	return this;
 }
 
 tQuery.Scene.prototype.fullpage	= function()
 {
+	// FIXME i dont like this function. way too cooked for tquery core stuff
+	// put it elsewhere ? in a plugin ?
+	
 	// Should that be in pluging
 	var domElement	= document.body;
 	domElement.style.margin		= "0";
@@ -84,6 +142,7 @@ tQuery.Scene.prototype.fullpage	= function()
 	domElement.style.overflow	= 'hidden';
 	return this.appendTo(domElement);
 }
+
 
 tQuery.Scene.prototype.appendTo	= function(domElement)
 {
