4e9bbfaa90cdbd708a787ae9846a5dea8b46dbe3
Jerome Etienne <jerome.etienne@gmail.com>
2012-02-01 15:55:26 +0100

    examples/domevent/ cleanup

--- examples/domevent/tquery.object3d.domevent.js
+++ examples/domevent/tquery.object3d.domevent.js
@@ -1,30 +1,31 @@
-tQuery.Object3D.register('on', function(eventType, callback){
+(function(){
+	var domEvent	= new THREEx.DomEvent();
 
-	// yuk!!!! workaround a bug
-	// ugly kludge to automatically set the camera in threex.domevent
-	// - better version would be to listen so events on tQuery
-	// - tQuery.on('cameraChange', function(world){
-	//   
-	//   })
-	tQuery.Object3D.prototype._tqDomEvent.camera(tQuery.world.camera());
-		
+	tQuery.Object3D.register('on', function(eventType, callback){
 
-
-	var domEvent	= this._tqDomEvent;
-	this.each(function(object3d){
-		domEvent.bind(object3d, eventType, callback, false);
+		// yuk!!!! workaround a bug
+		// ugly kludge to automatically set the camera in threex.domevent
+		// - see github issue #11
+		// - better version would be to listen so events on tQuery ?
+		// - tQuery.on('cameraChange', function(world){
+		//   
+		//   })
+		domEvent.camera(tQuery.world.camera());
+			
+	
+	
+		this.each(function(object3d){
+			domEvent.bind(object3d, eventType, callback, false);
+		});
+		return this;	// for chained API
 	});
-	return this;	// for chained API
-});
-
-tQuery.Object3D.register('off', function(eventType, callback){
-	var domEvent	= this._tqDomEvent;
-	this.each(function(object3d){
-		domEvent.unbind(object3d, eventType, callback, false);
+	
+	tQuery.Object3D.register('off', function(eventType, callback){
+		this.each(function(object3d){
+			domEvent.unbind(object3d, eventType, callback, false);
+		});
+		return this;	// for chained API
 	});
-	return this;	// for chained API
-});
-
-tQuery.Object3D.register('_tqDomEvent', new THREEx.DomEvent());
+})();
 
 
