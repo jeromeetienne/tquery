8316a3e42cb70893005deb4c0e180c4ae0ed9b8d
Jerome Etienne <jerome.etienne@gmail.com>
2012-02-12 19:58:16 +0100

    examples/requirejs cleanup. mostly lookable. try to go further ... ?

--- examples/requirejs/tquery.keyboard.js
+++ examples/requirejs/tquery.keyboard.js
@@ -2,20 +2,10 @@
  * tquery.js plugin to handle keyboard
 */
 
-var getBaseUrl	= function(filename){
-	//var scriptUrl	= document.scripts[document.scripts.length-1].src;
-	var scriptUrl	= document.querySelector('script[src$="'+filename+'"]').src;
-	var baseUrl	= scriptUrl.substr(0, scriptUrl.lastIndexOf('/')+1);
-	console.log("baseurl", baseUrl);
-	return baseUrl;
-}
-
-var baseurl	= getBaseUrl('tquery.keyboard.js');
-
-define('tquery.keyboard', ['tquery', baseurl+'../../vendor/threex/THREEx.KeyboardState.js'], function(){
+tQuery.define('tquery.keyboard', ['tquery', '../../vendor/threex/THREEx.KeyboardState.js'], function(){
 	var instance	= null;
 	tQuery.register('keyboard', function(){
 		if( !instance )	instance = new THREEx.KeyboardState();
 		return instance;
-	});	
+	});		
 });
--- examples/requirejs/tquery.requirejs.js
+++ examples/requirejs/tquery.requirejs.js
@@ -2,8 +2,36 @@
  * export tQuery as requirejs module
 */
 (function(){
+
+	var getScriptUrl	= function(filename){
+		var scriptUrl	= document.querySelector('script[src$="'+filename+'"]').src;
+		var baseUrl	= scriptUrl.substr(0, scriptUrl.lastIndexOf('/')+1);
+		//console.log("baseurl", baseUrl);
+		return baseUrl;
+	};
 	
+	var tQueryDefine	= function(moduleName, dependancies, callback){
+		var baseUrl	= getScriptUrl(moduleName+".js");
+		dependancies	= dependancies.map(function(item){
+			//console.log("item", item, baseUrl, item.match(/^\./))
+			var result;
+			if( item.match(/^\./) )	result	= baseUrl + item;
+			else			result	= item;
+			//console.log("item result", result)
+			return result;
+		})
+		// TODO pass baseUrl to the callback
+		// first param, all the rest are the one passed by requirejs
+		
+		//console.log("DDDbaseUrl", moduleName, baseUrl, dependancies)
+		define(moduleName, dependancies, callback);
+	};
+
+	window.tQuery	= { define: tQueryDefine };
+
 	define('tquery', ["../../build/tquery-bundle.js"], function(){
+		
+		tQuery.define	= tQueryDefine;
 		return tQuery;
 	});
 
