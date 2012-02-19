eea91a36b898c468fa848b12f68edcc3ad1ac2fc
Jerome Etienne <jerome.etienne@gmail.com>
2012-02-19 13:17:52 +0100

    examples/requirejs/ still working on requirejs... still no good

---
+++ examples/boilerplate2/tquery.world.boilerplate.js
@@ -0,0 +1,71 @@
+define(['module', 'tquery',
+	'../../../vendor/threex/THREEx.WindowResize',
+	'../../../vendor/threex/THREEx.screenshot',
+	'../../../vendor/threex/THREEx.FullScreen',
+	'../../../vendor/threex.dragpancontrols.js',
+	'../../../vendor/three.js/Stats.js'
+	], function(module){
+
+	tQuery.World.register('addBoilerplate', function(){
+		// sanity check - no boilerplate is already installed
+		console.assert( this.hasBoilerplate() !== true );
+	
+		// create the context
+		tQuery.data(this, '_boilerplateCtx', {});
+		// get the context
+		var ctx	= tQuery.data(this, "_boilerplateCtx");
+	
+		ctx.stats	= tQuery.createStats();
+	
+	
+		// get some variables
+		var camera	= this.camera();
+		var renderer	= this.renderer();
+	
+		// create a camera contol
+		ctx.cameraControls	= new THREEx.DragPanControls(camera)
+		ctx.loopCameraControls	= function(){
+			ctx.cameraControls.update();
+		};
+		this.loop().hook(ctx.loopCameraControls);
+	
+		// transparently support window resize
+		ctx.windowResize	= THREEx.WindowResize.bind(renderer, camera);
+		// allow 'p' to make screenshot
+		ctx.screenShot		= THREEx.Screenshot.bindKey(renderer);
+		// allow 'f' to go fullscreen where this feature is supported
+		if( THREEx.FullScreen.available() ){
+			ctx.fullscreen	= THREEx.FullScreen.bindKey();		
+		}
+		// for chained API
+		return this;
+	});
+	
+	tQuery.World.register('hasBoilerplate', function(){
+		// get the context
+		var ctx	= tQuery.data(this, "_boilerplateCtx")
+		// return true if ctx if defined, false otherwise
+		return ctx === undefined ? false : true;
+	});
+	
+	tQuery.World.register('removeBoilerplate', function(){
+		// get context
+		var ctx	= tQuery.data(this, '_boilerplateCtx');
+		// if not present, return now
+		if( ctx === undefined )	return	this;
+		// remove the context from this
+		tQuery.removeData(this, '_boilerplateCtx');
+	
+		ctx.stats.destroy();
+		// remove camera
+		this.loop().unhook(ctx.loopCameraControls)
+		// stop windowResize
+		ctx.windowResize.stop();
+		// unbind screenShot
+		ctx.screenShot.unbind();
+		// unbind fullscreen
+		ctx.fullscreen && ctx.fullscreen.unbind();
+		// for chained API
+		return this;
+	});
+});
