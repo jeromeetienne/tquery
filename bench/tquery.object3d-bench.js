c04c35713a20de0fc75e8de1d1e77b3a40c75fc2
Jerome Etienne <jerome.etienne@gmail.com>
2012-02-24 17:21:06 +0100

    bench/ new runner

--- bench/tquery.object3d-bench.js
+++ bench/tquery.object3d-bench.js
@@ -1,17 +1,72 @@
 // create a context
-var world	= tQuery.createWorld().fullpage().start();
+var world	= tQuery.createWorld();
 var object	= tQuery.createTorus().id('myId').addClass('myClass').addTo(world);
-world.stop();
 
-var suite = new Benchmark.Suite;
+var suite	= new Benchmark.Suite('Various Selectors');
 suite.add('geometry selector', function() {
 	tQuery('torus');
-}).add('id selector', function() {
+});
+
+suite.add('id selector', function() {
 	tQuery('#myId');
-}).add('class selector', function() {
+});
+
+suite.add('class selector', function() {
 	tQuery('#myClass');
-}).on('cycle', function(event, bench) {
+});
+
+suite.on('cycle', function(event, bench) {
 	console.log(String(bench));
-}).on('complete', function(){
+})
+
+suite.on('complete', function(){
 	console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-}).run({ 'async': true });
+	displaySuite(this);
+});
+
+suite.run({ 'async': true });
+
+console.dir(suite);
+
+
+
+suite.on('cycle', function(event, bench){
+	displaySuite(this);
+}).on('complete', function(event, bench){
+	displaySuite(this);
+});
+
+
+var displaySuite	= function(suite){
+	jQuery('#benchOutput').empty();
+
+	var templateStr	= jQuery('#suiteHead').text().trim();
+	var result	= _.template(templateStr, {
+		name	: suite.name,
+		running	: suite.running
+	});
+	jQuery(result).appendTo('#benchOutput')	
+
+
+	// compute hzMax
+	var hzMax	= 0;
+	suite.forEach(function(bench, idx){
+		hzMax	= Math.max(hzMax, bench.hz)
+	});
+
+	suite.forEach(function(bench, idx){
+		//console.log("kkk", bench.hz, 'kkk');
+		var context	= {
+			name	: bench.name,
+			hz	: bench.hz,
+			ratio	: (bench.hz > 0 && hzMax > 0) ? bench.hz / hzMax : null
+		};
+		var templateStr	= jQuery('#benchItem').text().trim();
+		var result	= _.template(templateStr, context);
+		jQuery(result).appendTo('#benchOutput')	
+	});	
+}
+
+displaySuite(suite);
+
+
