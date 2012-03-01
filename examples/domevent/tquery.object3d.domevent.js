81a268c5e04a32d8690dbd18d28201d512f855e5
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-03-02 01:08:28 +0200

    Fixed linkify example

--- examples/domevent/tquery.object3d.domevent.js
+++ examples/domevent/tquery.object3d.domevent.js
@@ -1,26 +1,9 @@
 /**
  * plugin to handle deviceOrientation API
 */
-define(['/examples/domevent/threex.domevent.js'], function(){
-	var domEvent;
-	var getInstance	= function(){
-		if( !domEvent ){
-			domEvent	= new THREEx.DomEvent();
-			// yuk!!!! workaround a bug
-			// ugly kludge to automatically set the camera in threex.domevent
-			// - see github issue #11
-			// - better version would be to listen so events on tQuery ?
-			// - tQuery.on('cameraChange', function(world){
-			//   
-			//   })
-			domEvent.camera(tQuery.world.camera());
-		}
-		return domEvent
-	}
-	
-	tQuery.Object3D.register('on', function(eventType, callback){
-		var domEvent	= getInstance();
+define(['examples/domevent/threex.domevent'], function(domEvent){
 
+	tQuery.Object3D.register('on', function(eventType, callback){
 		this.each(function(object3d){
 			domEvent.bind(object3d, eventType, callback, false);
 		});
@@ -28,7 +11,6 @@ define(['/examples/domevent/threex.domevent.js'], function(){
 	});
 	
 	tQuery.Object3D.register('off', function(eventType, callback){
-		var domEvent	= getInstance();
 		this.each(function(object3d){
 			domEvent.unbind(object3d, eventType, callback, false);
 		});
--- examples/text/tquery.text.js
+++ examples/text/tquery.text.js
@@ -1,17 +1,7 @@
 /**
  * Create tQuery.Scene
 */
-define(["fonts/gentilis_bold.typeface.js",
-	"fonts/gentilis_regular.typeface.js",
-	"fonts/optimer_bold.typeface.js",
-	"fonts/optimer_regular.typeface.js",
-	"fonts/helvetiker_bold.typeface.js",
-	"fonts/helvetiker_regular.typeface.js",
-	"fonts/droid/droid_sans_regular.typeface.js",
-	"fonts/droid/droid_sans_bold.typeface.js",
-	"fonts/droid/droid_serif_regular.typeface.js",
-	"fonts/droid/droid_serif_bold.typeface.js"
-	], function(){
+define([], function(){
 	tQuery.register('createText', function(text, options){
 		// handle parameters
 		options	= tQuery.extend(options, {
