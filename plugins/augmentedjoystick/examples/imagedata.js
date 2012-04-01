d2f92323064152cdb90dd084d6eb72f1a8affde4
Jerome Etienne <jerome.etienne@gmail.com>
2012-04-01 09:19:43 +0200

    a LOT of thing. fix #126 among them

--- plugins/augmentedjoystick/examples/imagedata.js
+++ plugins/augmentedjoystick/examples/imagedata.js
@@ -32,12 +32,11 @@ ImageData.fliph	= function(imageData)
 
 ImageData.luminance	= function(imageData, ratio)
 {
+	ratio	= ratio !== undefined ? ratio : 1.0;
 	var p	= imageData.data;
 	var w	= imageData.width;
 	var h	= imageData.height;
-	var i	= 0;
-	ratio	= ratio !== undefined ? ratio : 1.0;
-	for(var y = 0; y < h; y++){
+	for(var i = 0, y = 0; y < h; y++){
 		for(var x = 0; x < w; x++, i += 4){
 			var luminance	= (0.2126*(p[i+0]/255)) + (0.7152*(p[i+1]/255)) + (0.0722*(p[i+2]/255))
 			luminance	= Math.floor(luminance*ratio*255);
@@ -47,7 +46,6 @@ ImageData.luminance	= function(imageData, ratio)
 		}
 	}
 }
-
 
 ImageData.duplicate	= function(srcImageData, ctx)
 {
@@ -64,7 +62,24 @@ ImageData.duplicate	= function(srcImageData, ctx)
 //										//
 //////////////////////////////////////////////////////////////////////////////////
 
-ImageData.threshold	= function(imageData, min, max)
+ImageData.threshold	= function(imageData, r, g, b)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	for(var i = 0, y = 0; y < h; y++){
+		for(var x = 0; x < w; x++, i += 4){
+			if( (p[i+0] >= r.min && p[i+0] <= r.max)
+					&& (p[i+1] >= g.min && p[i+1] <= g.max)
+					&& (p[i+2] >= b.min && p[i+2] <= b.max) ){
+			}else{
+				p[i+0]	= p[i+1] = p[i+2] = 0;
+			}
+		}
+	}
+}
+
+ImageData.convert	= function(imageData, callback)
 {
 	var p	= imageData.data;
 	var w	= imageData.width;
@@ -72,54 +87,212 @@ ImageData.threshold	= function(imageData, min, max)
 	var i	= 0;
 	for(var y = 0; y < h; y++){
 		for(var x = 0; x < w; x++, i += 4){
-			if( p[i+0] >= min.r && p[i+0] <= max.r )	continue;
-			if( p[i+1] >= min.g && p[i+1] <= max.g )	continue;
-			if( p[i+2] >= min.b && p[i+2] <= max.b )	continue;
-			if( p[i+3] >= min.a && p[i+3] <= max.a )	continue;
-			p[i+0]	= p[i+1] = p[i+2] = p[i+3] = 0;
+			callback(p, i, x, y, imageData);
 		}
 	}
 }
 
-ImageData.greenish	= function(imageData)
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+ImageData.hline	= function(imageData, y, r, g, b, a)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var i	= y * w * 4;
+	r	= r !== undefined ? r : 255;
+	g	= g !== undefined ? g : 255;
+	b	= b !== undefined ? b : 255;
+	a	= a !== undefined ? a : 255;
+	for(var x = 0; x < w; x++, i += 4){
+		p[i+0]	= r;
+		p[i+1]	= g;
+		p[i+2]	= b;
+		p[i+3]	= a;
+	}
+}
+
+ImageData.vline	= function(imageData, x, r, g, b, a)
 {
 	var p	= imageData.data;
 	var w	= imageData.width;
 	var h	= imageData.height;
-	var i	= 0;
-	for(var y = 0; y < h; y++){
-		for(var x = 0; x < w; x++, i += 4){
-			//if( p[i+0] <= 30 )	continue;
-			//if( p[i+2] <= 30 )	continue;
-
-			var sumRB	= p[i+0]+p[i+2];
-
-			if( p[i+1] >= 130 && sumRB < 300 )	continue;
-
-			p[i+0]	= p[i+1] = p[i+2] = p[i+3] = 0;
-		}
+	r	= r !== undefined ? r : 255;
+	g	= g !== undefined ? g : 255;
+	b	= b !== undefined ? b : 255;
+	a	= a !== undefined ? a : 255;
+	for(var i = x*4, y = 0; y < h; y++, i += w*4){
+		p[i+0]	= r;
+		p[i+1]	= g;
+		p[i+2]	= b;
+		p[i+3]	= a;
 	}
 }
 
-ImageData.blueish	= function(imageData)
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+ImageData.greenish	= function(imageData, threshold)
 {
 	var p	= imageData.data;
 	var w	= imageData.width;
 	var h	= imageData.height;
-	var i	= 0;
-	for(var y = 0; y < h; y++){
+	console.assert(threshold, "invalid parameter")
+	for(var i = 0, y = 0; y < h; y++){
 		for(var x = 0; x < w; x++, i += 4){
-			//if( p[i+0] <= 30 )	continue;
-			//if( p[i+2] <= 30 )	continue;
-
-			var sumRG	= p[i+0]+p[i+1];
-
-			if( p[i+2] >= 130 && sumRG < 200 )	continue;
-
+			var maxSum	= p[i+0]+p[i+2];
+			if( p[i+1] >= threshold.minCol && maxSum <= threshold.maxSum  )	continue;
 			p[i+0]	= p[i+1] = p[i+2] = p[i+3] = 0;
 		}
 	}
 }
+
+ImageData.blueish	= function(imageData, threshold)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	console.assert(threshold, "invalid parameter")
+	for(var i = 0, y = 0; y < h; y++){
+		for(var x = 0; x < w; x++, i += 4){
+			var maxSum	= p[i+0]+p[i+1];
+			if( p[i+1] >= threshold.minCol && maxSum <= threshold.maxSum  )	continue;
+			p[i+0]	= p[i+1] = p[i+2] = p[i+3] = 0;
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+ImageData.smoothHistogram	= function(hist, factor)
+{
+	var value	= 0;
+	for(var i = 0; i < hist.length; i++ ){
+		value	+= (hist[i] - value) * factor;
+		hist[i]	= value;
+	}
+}
+
+ImageData.getMaxHistogram	= function(hist, imageData)
+{
+	var max	= -Number.MAX_VALUE;
+	var idx	= 0;
+	for(var i = 0; i < hist.length; i++ ){
+		if( hist[i] > max ){
+			max	= hist[i];
+			idx	= i;
+		}
+	}
+
+	return {max: max, idx: idx}
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+ImageData.computeVerticalHistogram	= function(imageData, filter)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	var hist= new Float64Array(w);
+	console.assert(filter, "invalid parameter")
+	for(var x = 0; x < w; x++){
+		for(var i = x*4, y = 0; y < h; y++, i += w*4){
+			if( filter(p, i, x, y, imageData) )	hist[x]++;
+		}
+	}
+	return hist;
+}
+
+ImageData.displayVerticalHistogram	= function(imageData, hist)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	console.assert(hist.length === w);
+	// create temporary cancas
+	var canvas	= document.createElement('canvas');
+	canvas.width	= w;
+	canvas.height	= h;
+	var ctx		= canvas.getContext("2d");
+	// display the histogram in the canvas
+	ctx.fillStyle	= 'red';
+	for(var x = 0; x < w; x++ ){
+		ctx.fillRect(x, h-hist[x], 1, hist[x]);
+	}
+	// copy the canvas in imageData
+	var srcImgData	= ctx.getImageData(0,0, canvas.width, canvas.height);
+	var pSrc	= srcImgData.data;
+	var pDst	= imageData.data;
+	for(var i = 0; i < pSrc.length; i += 4){
+		if( pSrc[i+0] !== 0 || pSrc[i+1] !== 0 || pSrc[i+2] !== 0 ){
+			pDst[i+0]	= pSrc[i+0];
+			pDst[i+1]	= pSrc[i+1];
+			pDst[i+2]	= pSrc[i+2];
+		}
+		pDst[i+3]	= 255;
+	}
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+ImageData.computeHorizontalHistogram	= function(imageData, filter)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	var hist= new Float64Array(h);
+	console.assert(filter, "invalid parameter")
+	for(var i = 0, y = 0; y < h; y++){
+		for(var x = 0; x < w; x++, i += 4){
+			if( filter(p, i, x, y, imageData) )	hist[y]++;
+		}
+	}
+	return hist;
+}
+
+ImageData.displayHorizontalHistogram	= function(imageData, hist)
+{
+	var p	= imageData.data;
+	var w	= imageData.width;
+	var h	= imageData.height;
+	console.assert(hist.length === h);
+	// create temporary cancas
+	var canvas	= document.createElement('canvas');
+	canvas.width	= w;
+	canvas.height	= h;
+	var ctx		= canvas.getContext("2d");
+	// display the histogram in the canvas
+	ctx.fillStyle	= 'red';
+	for(var y = 0; y < h; y++ ){
+		ctx.fillRect(0, y, hist[y], 1);
+	}
+	// copy the canvas in imageData
+	var srcImgData	= ctx.getImageData(0,0, canvas.width, canvas.height);
+	var pSrc	= srcImgData.data;
+	var pDst	= imageData.data;
+	for(var i = 0; i < pSrc.length; i += 4){
+		if( pSrc[i+0] !== 0 || pSrc[i+1] !== 0 || pSrc[i+2] !== 0 ){
+			pDst[i+0]	= pSrc[i+0];
+			pDst[i+1]	= pSrc[i+1];
+			pDst[i+2]	= pSrc[i+2];
+		}
+		pDst[i+3]	= 255;
+	}
+}
+
 
 //////////////////////////////////////////////////////////////////////////////////
 //										//
@@ -133,17 +306,14 @@ ImageData.computeColorHistogram	= function(imageData)
 	var hist= {
 		r	: new Float64Array(256),
 		g	: new Float64Array(256),
-		b	: new Float64Array(256),
-		a	: new Float64Array(256)
+		b	: new Float64Array(256)
 	};
 	
-	var i	= 0;
-	for(var y = 0; y < h; y++){
+	for(var i = 0, y = 0; y < h; y++){
 		for(var x = 0; x < w; x++, i += 4){
 			hist.r[ p[i+0] ]++;
 			hist.g[ p[i+1] ]++;
 			hist.b[ p[i+2] ]++;
-			hist.a[ p[i+3] ]++;
 		}
 	}
 	return hist;
@@ -157,11 +327,49 @@ ImageData.normalizeColorHistogram	= function(colorHistogram)
 	var max	= -Number.MAX_VALUE;
 	for (var i = 0; i < 256; i++ ){
 		max	= Math.max(max, hist.r[i]);
+		max	= Math.max(max, hist.g[i]);
+		max	= Math.max(max, hist.b[i]);
 	}
 	// normalize the histogram
 	for (var i = 0; i < 256; i++ ){
 		hist.r[i]	/= max;
+		hist.g[i]	/= max;
+		hist.b[i]	/= max;
 	}
+
+	var sumR	= 0;
+	var sumG	= 0;
+	var sumB	= 0;
+	for (var i = 0; i < 256; i++ ){
+		sumR	+= hist.r[i];
+		sumG	+= hist.g[i];
+		sumB	+= hist.b[i];
+	}
+	console.log("sum", max, sumR, sumG, sumB)
+}
+
+ImageData.normalizeColorHistogram0	= function(colorHistogram)
+{
+	var hist= colorHistogram;
+
+	// get the max value
+	var max	= -Number.MAX_VALUE;
+	for (var i = 0; i < 256; i++ )	max		= Math.max(max, hist.r[i]);
+	for (var i = 0; i < 256; i++ )	hist.r[i]	/= max;
+	var max	= -Number.MAX_VALUE;
+	for (var i = 0; i < 256; i++ )	max		= Math.max(max, hist.g[i]);
+	for (var i = 0; i < 256; i++ )	hist.g[i]	/= max;
+	var max	= -Number.MAX_VALUE;
+	for (var i = 0; i < 256; i++ )	max		= Math.max(max, hist.b[i]);
+	for (var i = 0; i < 256; i++ )	hist.b[i]	/= max;
+
+	var sumR = 0, sumG = 0, sumB = 0;
+	for (var i = 0; i < 256; i++ ){
+		sumR	+= hist.r[i];
+		sumG	+= hist.g[i];
+		sumB	+= hist.b[i];
+	}
+	console.log("sum", max, sumR, sumG, sumB)
 }
 
 ImageData.displayColorHistogram	= function(imageData, colorHistogram)
@@ -175,23 +383,40 @@ ImageData.displayColorHistogram	= function(imageData, colorHistogram)
 	canvas.width	= w;
 	canvas.height	= h;
 	var ctx		= canvas.getContext("2d");
-	ctx.fillStyle	= 'red';
+
 
 	var barW	= canvas.width / 256;
 	var barH	= canvas.height / 3;
-	var barYOffset	= 0;
+
+	ctx.fillStyle	= 'red';
+	var barYOffset	= 0 * barH;
 	for (var i = 0; i < 256; i++ ){
-		ctx.fillRect(
-			i*barW,		// x
-			barYOffset,	// y
-			barW,				// w
-			Math.floor(hist.r[i]*256*barH)	// h
-		);
+		var valH	= Math.floor(hist.r[i]*barH);
+		ctx.fillRect(i*barW, barYOffset+(barH-valH), barW, valH);
 	}
+
+	ctx.fillStyle	= 'green';
+	var barYOffset	= 1 * barH;
+	for (var i = 0; i < 256; i++ ){
+		var valH	= Math.floor(hist.g[i]*barH);
+		ctx.fillRect(i*barW, barYOffset+(barH-valH), barW, valH);
+	}
+
+	ctx.fillStyle	= 'blue';
+	var barYOffset	= 2 * barH;
+	for (var i = 0; i < 256; i++ ){
+		var valH	= Math.floor(hist.b[i]*barH);
+		ctx.fillRect(i*barW, barYOffset+(barH-valH), barW, valH);
+	}
+
 	var srcImgData	= ctx.getImageData(0,0, canvas.width, canvas.height);
 	var pSrc	= srcImgData.data;
 	var pDst	= imageData.data;
-	for(var i = 0; i < pSrc.length; i++){
-		pDst[i]	= pSrc[i];
+	for(var i = 0; i < pSrc.length; i += 4){
+		if( pSrc[i+0] !== 0 || pSrc[i+1] !== 0 || pSrc[i+2] !== 0 ){
+			pDst[i+0]	= pSrc[i+0];
+			pDst[i+1]	= pSrc[i+1];
+			pDst[i+2]	= pSrc[i+2];
+		}
 	}
 }
--- plugins/augmentedjoystick/tquery.createaugmentedjoystick.js
+++ plugins/augmentedjoystick/tquery.createaugmentedjoystick.js
@@ -30,46 +30,77 @@ tQuery.register('createAugmentedJoystick', function(opts){
 	canvas.width	= video.width	/4;
 	canvas.height	= video.height	/4;
 	var ctx		= canvas.getContext("2d");
-	//var texture	= new THREE.Texture( video );
 	var texture	= new THREE.Texture( canvas );
 
-window.canvas	= canvas;
-window.ctx	= ctx;
-window.video	= video;
-window.imageData= ctx.getImageData(0,0, canvas.width, canvas.height);
-
-	var imageData	= ctx.getImageData(0,0, canvas.width, canvas.height);
-	console.log("imageData", imageData)
-	
 	var frameCounter	= 0;
 	var frameRate		= 1;
 	opts.loop.hook(function(){
+		// rate limiter
 		frameCounter++;
 		if( frameCounter % frameRate !== 0 )	return;
+		// if no data is ready, do nothing
+		if( video.readyState !== video.HAVE_ENOUGH_DATA )	return;
+		// draw video into a canvas2D
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-		if( video.readyState === video.HAVE_ENOUGH_DATA ){
-			
-			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+		var imageData	= ctx.getImageData(0,0, canvas.width, canvas.height);
 
-			var imageData	= ctx.getImageData(0,0, canvas.width, canvas.height);
+		ImageData.fliph(imageData);
+		//ImageData.luminance(imageData);
 
-			//ImageData.fliph(imageData);
-			//ImageData.luminance(imageData);
+// Green
+		var greenData	= ImageData.duplicate(imageData, ctx);
+		ImageData.threshold(greenData, guiOpts.green.threshold.r, guiOpts.green.threshold.g, guiOpts.green.threshold.b);
+		if( guiOpts.green.disp.enable )	imageData	= greenData;
+		// horizontal coord Y discovery
+		var hist	= ImageData.computeHorizontalHistogram(greenData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.smoothHistogram(hist, guiOpts.green.smooth.hFactor);
+		var maxHGreen	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.green.disp.HHist )	ImageData.displayHorizontalHistogram(imageData, hist);
+		
+		// horizontal coord X discovery
+		var hist	= ImageData.computeVerticalHistogram(greenData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.smoothHistogram(hist, guiOpts.green.smooth.vFactor);
+		var maxVGreen	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.green.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 
-			//var greenData	= ImageData.duplicate(imageData, ctx);
-			//ImageData.greenish(greenData);
 
-			//var blueData	= ImageData.duplicate(imageData, ctx);
-			//ImageData.greenish(imageData);
-			//ImageData.blueish(blueData);
-			//var colorHist	= ImageData.computeColorHistogram(blueData);
-			//ImageData.normalizeColorHistogram(colorHist);
-			//ImageData.displayColorHistogram(imageData, colorHist);
+// Blue
+		var blueData	= ImageData.duplicate(imageData, ctx);
+		ImageData.threshold(blueData, guiOpts.blue.threshold.r, guiOpts.blue.threshold.g, guiOpts.blue.threshold.b);
+		if( guiOpts.blue.disp.enable )	imageData	= blueData;
+		// horizontal coord Y discovery
+		var hist	= ImageData.computeHorizontalHistogram(blueData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.smoothHistogram(hist, guiOpts.blue.smooth.hFactor);
+		var maxHBlue	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.blue.disp.HHist )	ImageData.displayHorizontalHistogram(imageData, hist);
+		
+		// horizontal coord X discovery
+		var hist	= ImageData.computeVerticalHistogram(blueData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.smoothHistogram(hist, guiOpts.blue.smooth.vFactor);
+		var maxVBlue	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.blue.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 
-			ctx.putImageData(imageData, 0, 0);
-			
-			texture.needsUpdate	= true;
-		}
+// Display Crosses
+		// green
+		if( guiOpts.green.disp.HLine )	ImageData.hline(imageData, maxHGreen.idx, 0, 255, 0);
+		if( guiOpts.green.disp.VLine )	ImageData.vline(imageData, maxVGreen.idx, 0, 255, 0);
+		// blue
+		if( guiOpts.blue.disp.HLine )	ImageData.hline(imageData, maxHBlue.idx, 0, 0, 255);
+		if( guiOpts.blue.disp.VLine )	ImageData.vline(imageData, maxVBlue.idx, 0, 0, 255);
+
+		// update the canvas
+		ctx.putImageData(imageData, 0, 0);
+		// mark the texture as needsUpdate
+		texture.needsUpdate	= true;
 	});
 	
 	return texture;
