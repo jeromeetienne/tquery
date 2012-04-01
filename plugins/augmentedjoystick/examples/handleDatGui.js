2462643559057c396223d28eacdb494654e03e27
Jerome Etienne <jerome.etienne@gmail.com>
2012-04-01 18:11:31 +0200

    basic gesture reco + pointer position smoother  plugins/augmentedjoystick

--- plugins/augmentedjoystick/examples/handleDatGui.js
+++ plugins/augmentedjoystick/examples/handleDatGui.js
@@ -1,56 +1,72 @@
 var DatGuiOpts	= function(){
+	this.general	= {
+		video	: {
+			w	: 320/4,
+			h	: 240/4
+		}
+	};
 	this.right	= {
+		pointer	: {
+			display		: true,
+			coordSmoothV	: 0.3,
+			coordSmoothH	: 0.3
+		},
 		disp	: {
 			enable	: false,
-			HHist	: false,
 			VHist	: false,
-			HLine	: false,
-			VLine	: false
+			HHist	: false,
+			VLine	: false,
+			HLine	: false
 		},
 		threshold	: {
 			r	: {
-				min	:   0,
-				max	: 130
+				min	: 140,
+				max	: 255
 			},
 			g	: {
-				min	:   0,
-				max	: 150
+				min	:  41,
+				max	: 140
 			},
 			b	: {
-				min	: 95,
-				max	: 255
+				min	:  10,
+				max	: 100
 			}
 		},
 		smooth	: {
-			hWidth	: 9,
-			vWidth	: 9
+			vWidth	: 9,
+			hWidth	: 9
 		}
 	};
 	this.left	= {
+		pointer	: {
+			display		: true,
+			coordSmoothV	: 0.3,
+			coordSmoothH	: 0.3
+		},
 		disp	: {
-			enable	: true,
-			HHist	: true,
-			VHist	: true,
-			HLine	: true,
-			VLine	: true
+			enable	: false,
+			VHist	: false,
+			HHist	: false,
+			VLine	: false,
+			HLine	: false
 		},
 		threshold	: {
 			r	: {
 				min	:   0,
-				max	: 130
+				max	: 145
 			},
 			g	: {
-				min	: 110,
+				min	: 145,
 				max	: 255
 			},
 			b	: {
 				min	:   0,
-				max	: 130
+				max	: 155
 			}
 		},
 		smooth	: {
-			hWidth	: 9,
-			vWidth	: 9
+			vWidth	: 9,
+			hWidth	: 9
 		}
 	};
 };
@@ -59,14 +75,25 @@ var guiOpts	= new DatGuiOpts();
 
 window.addEventListener('load', function(){
 	var gui		= new dat.GUI();
+// General folder
+	var folder	= gui.addFolder('General');
+	//folder.open();
+	folder.add(guiOpts.general.video, 'w', 0, 320).step(40).name('videoW');
+	folder.add(guiOpts.general.video, 'h', 0, 240).step(40).name('videoH');
+
+// Right pointer folder
+	var folder	= gui.addFolder('Right Pointer');
+	folder.add(guiOpts.right.pointer	, 'display');
+	folder.add(guiOpts.right.pointer	, 'coordSmoothV', 0, 1);
+	folder.add(guiOpts.right.pointer	, 'coordSmoothH', 0, 1);
 // Right display folder
 	var folder	= gui.addFolder('Right Display');
 	//folder.open();
-	folder.add(guiOpts.right.disp, 'enable');
-	folder.add(guiOpts.right.disp, 'HHist');
-	folder.add(guiOpts.right.disp, 'VHist');
-	folder.add(guiOpts.right.disp, 'HLine');
-	folder.add(guiOpts.right.disp, 'VLine');
+	folder.add(guiOpts.right.disp	, 'enable');
+	folder.add(guiOpts.right.disp	, 'HHist');
+	folder.add(guiOpts.right.disp	, 'VHist');
+	folder.add(guiOpts.right.disp	, 'HLine');
+	folder.add(guiOpts.right.disp	, 'VLine');
 // Right Threshold folder
 	var folder	= gui.addFolder('Right Threshold');
 	//folder.open();
@@ -79,14 +106,19 @@ window.addEventListener('load', function(){
 	folder.add(guiOpts.right.smooth		, 'hWidth', 0, 20).step(1);
 	folder.add(guiOpts.right.smooth		, 'vWidth', 0, 20).step(1);
 
+// Left pointer folder
+	var folder	= gui.addFolder('Left Pointer');
+	folder.add(guiOpts.left.pointer	, 'display');
+	folder.add(guiOpts.left.pointer	, 'coordSmoothV', 0, 1);
+	folder.add(guiOpts.left.pointer	, 'coordSmoothH', 0, 1);
 // Left display folder
 	var folder	= gui.addFolder('Left Display');
 	//folder.open();
-	folder.add(guiOpts.left.disp, 'enable');
-	folder.add(guiOpts.left.disp, 'HHist');
-	folder.add(guiOpts.left.disp, 'VHist');
-	folder.add(guiOpts.left.disp, 'HLine');
-	folder.add(guiOpts.left.disp, 'VLine');
+	folder.add(guiOpts.left.disp	, 'enable');
+	folder.add(guiOpts.left.disp	, 'VHist');
+	folder.add(guiOpts.left.disp	, 'HHist');
+	folder.add(guiOpts.left.disp	, 'VLine');
+	folder.add(guiOpts.left.disp	, 'HLine');
 // Left Threshold folder
 	var folder	= gui.addFolder('Left Threshold');
 	//folder.open();
@@ -96,8 +128,8 @@ window.addEventListener('load', function(){
 	folder.add(guiOpts.left.threshold.g	, 'max', 0, 255).name('green max');
 	folder.add(guiOpts.left.threshold.b	, 'min', 0, 255).name('blue min');
 	folder.add(guiOpts.left.threshold.b	, 'max', 0, 255).name('blue max');
-	folder.add(guiOpts.left.smooth		, 'hWidth', 0, 20).step(1);
 	folder.add(guiOpts.left.smooth		, 'vWidth', 0, 20).step(1);
+	folder.add(guiOpts.left.smooth		, 'hWidth', 0, 20).step(1);
 
 // try to save value but doesnt work
 	//gui.remember(guiOpts);
--- plugins/augmentedjoystick/tquery.createaugmentedjoystick.js
+++ plugins/augmentedjoystick/tquery.createaugmentedjoystick.js
@@ -1,7 +1,12 @@
+tQuery.register('createAugmentedJoystick', function(opts){
+	return new tQuery.AugmentedJoystick(opts);
+});
+
+
 /**
  * Create tQuery.Scene
 */
-tQuery.register('createAugmentedJoystick', function(opts){
+tQuery.register('AugmentedJoystick', function(opts){
 	// handle parameters
 	opts	= tQuery.extend(opts, {
 		loop	: tQuery.world.loop()
@@ -15,23 +20,33 @@ tQuery.register('createAugmentedJoystick', function(opts){
 	var hasUserMedia = navigator.webkitGetUserMedia ? true : false;
 	//console.log("UserMedia is detected", hasUserMedia);
 
-	if( hasUserMedia ){
-		navigator.webkitGetUserMedia('video', function(stream){
-			video.src	= webkitURL.createObjectURL(stream);
-			//console.log("pseudo object URL", video.src);
-		}, function(error){
-			alert('you got no WebRTC webcam');
-		});
-	}else{
-		console.assert(false);
-	}
+	if( !tQuery.AugmentedJoystick.hasUserMedia )	alert('Panic: no UserMedia')
+	console.assert( tQuery.AugmentedJoystick.hasUserMedia, "no usermedia available");
 
+	navigator.webkitGetUserMedia('video', function(stream){
+		video.src	= webkitURL.createObjectURL(stream);
+		//console.log("pseudo object URL", video.src);
+	}, function(error){
+		alert('you got no WebRTC webcam');
+	});
 	var canvas	= document.createElement('canvas');
-	canvas.width	= video.width	/4;
-	canvas.height	= video.height	/4;
+	canvas.width	= guiOpts.general.video.w;
+	canvas.height	= guiOpts.general.video.h;
 	var ctx		= canvas.getContext("2d");
 	var texture	= new THREE.Texture( canvas );
 
+	// gesture recognition
+	var gestureR	= new tQuery.GestureRecognition();
+	var pointerR	= {
+		x	: canvas.width/2,
+		y	: canvas.height/2
+	};
+	var gestureL	= new tQuery.GestureRecognition();
+	var pointerL	= {
+		x	: canvas.width/2,
+		y	: canvas.height/2
+	};
+	
 	var frameCounter	= 0;
 	var frameRate		= 1;
 	opts.loop.hook(function(){
@@ -40,6 +55,11 @@ tQuery.register('createAugmentedJoystick', function(opts){
 		if( frameCounter % frameRate !== 0 )	return;
 		// if no data is ready, do nothing
 		if( video.readyState !== video.HAVE_ENOUGH_DATA )	return;
+		
+		// update canvas size if needed
+		if( canvas.width != guiOpts.general.video.w )	canvas.width	= guiOpts.general.video.w;
+		if( canvas.height != guiOpts.general.video.h )	canvas.height	= guiOpts.general.video.h;
+		
 		// draw video into a canvas2D
 		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
@@ -49,29 +69,36 @@ tQuery.register('createAugmentedJoystick', function(opts){
 		//ImageData.luminance(imageData);
 
 // Right
-		var blueData	= ImageData.duplicate(imageData, ctx);
-		ImageData.threshold(blueData, guiOpts.right.threshold.r, guiOpts.right.threshold.g, guiOpts.right.threshold.b);
-		if( guiOpts.right.disp.enable )	imageData	= blueData;
+		var rightData	= ImageData.duplicate(imageData, ctx);
+		ImageData.threshold(rightData, guiOpts.right.threshold.r, guiOpts.right.threshold.g, guiOpts.right.threshold.b);
+		if( guiOpts.right.disp.enable )	imageData	= rightData;
+		// horizontal coord X discovery
+		var hist	= ImageData.computeVerticalHistogram(rightData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.windowedAverageHistogram(hist, guiOpts.right.smooth.vWidth);
+		var maxVRight	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.right.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 		// horizontal coord Y discovery
-		var hist	= ImageData.computeHorizontalHistogram(blueData, function(p, i){
+		var hist	= ImageData.computeHorizontalHistogram(rightData, function(p, i){
 			return p[i+1] !== 0 ? true : false;
 		});
 		ImageData.windowedAverageHistogram(hist, guiOpts.right.smooth.hWidth);
 		var maxHRight	= ImageData.getMaxHistogram(hist);
 		if( guiOpts.right.disp.HHist )	ImageData.displayHorizontalHistogram(imageData, hist);
 		
-		// horizontal coord X discovery
-		var hist	= ImageData.computeVerticalHistogram(blueData, function(p, i){
-			return p[i+1] !== 0 ? true : false;
-		});
-		ImageData.windowedAverageHistogram(hist, guiOpts.right.smooth.vWidth);
-		var maxVRight	= ImageData.getMaxHistogram(hist);
-		if( guiOpts.right.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 
 // Left
 		var leftData	= ImageData.duplicate(imageData, ctx);
 		ImageData.threshold(leftData, guiOpts.left.threshold.r, guiOpts.left.threshold.g, guiOpts.left.threshold.b);
 		if( guiOpts.left.disp.enable )	imageData	= leftData;
+		// horizontal coord X discovery
+		var hist	= ImageData.computeVerticalHistogram(leftData, function(p, i){
+			return p[i+1] !== 0 ? true : false;
+		});
+		ImageData.windowedAverageHistogram(hist, guiOpts.left.smooth.vWidth);
+		var maxVLeft	= ImageData.getMaxHistogram(hist);
+		if( guiOpts.left.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 		// horizontal coord Y discovery
 		var hist	= ImageData.computeHorizontalHistogram(leftData, function(p, i){
 			return p[i+1] !== 0 ? true : false;
@@ -80,21 +107,34 @@ tQuery.register('createAugmentedJoystick', function(opts){
 		var maxHLeft	= ImageData.getMaxHistogram(hist);
 		if( guiOpts.left.disp.HHist )	ImageData.displayHorizontalHistogram(imageData, hist);
 		
-		// horizontal coord X discovery
-		var hist	= ImageData.computeVerticalHistogram(leftData, function(p, i){
-			return p[i+1] !== 0 ? true : false;
-		});
-		ImageData.windowedAverageHistogram(hist, guiOpts.left.smooth.vWidth);
-		var maxVLeft	= ImageData.getMaxHistogram(hist);
-		if( guiOpts.left.disp.VHist )	ImageData.displayVerticalHistogram(imageData, hist);
 
 // Display Crosses
 		// right
-		if( guiOpts.right.disp.HLine )	ImageData.hline(imageData, maxHRight.idx, 0, 0, 255);
 		if( guiOpts.right.disp.VLine )	ImageData.vline(imageData, maxVRight.idx, 0, 0, 255);
+		if( guiOpts.right.disp.HLine )	ImageData.hline(imageData, maxHRight.idx, 0, 0, 255);
 		// left
-		if( guiOpts.left.disp.HLine )	ImageData.hline(imageData, maxHLeft.idx, 0, 255, 0);
 		if( guiOpts.left.disp.VLine )	ImageData.vline(imageData, maxVLeft.idx, 0, 255, 0);
+		if( guiOpts.left.disp.HLine )	ImageData.hline(imageData, maxHLeft.idx, 0, 255, 0);
+
+
+// pointer Right
+		pointerR.x	+= (maxVRight.idx - pointerR.x) * guiOpts.right.pointer.coordSmoothV;
+		pointerR.y	+= (maxHRight.idx - pointerR.y) * guiOpts.right.pointer.coordSmoothH;
+		var eventR	= gestureR.update(pointerR.x, pointerR.y, canvas.width, canvas.height);
+		if( eventR )	console.log("gestureR event", eventR)
+		if( guiOpts.right.pointer.display ){
+			ImageData.vline(imageData, Math.floor(pointerR.x), 255, 0, 255);
+			ImageData.hline(imageData, Math.floor(pointerR.y), 255, 0, 255);
+		}
+// pointer Left
+		pointerL.x	+= (maxVLeft.idx - pointerL.x) * guiOpts.left.pointer.coordSmoothV;
+		pointerL.y	+= (maxHLeft.idx - pointerL.y) * guiOpts.left.pointer.coordSmoothH;
+		var eventL	= gestureL.update(pointerL.x, pointerL.y, canvas.width, canvas.height);
+		if( eventL )	console.log("gestureL event", eventL)
+		if( guiOpts.left.pointer.display ){
+			ImageData.vline(imageData, Math.floor(pointerL.x), 255, 0, 0);
+			ImageData.hline(imageData, Math.floor(pointerL.y), 255, 0, 0);
+		}
 
 		// update the canvas
 		ctx.putImageData(imageData, 0, 0);
@@ -105,3 +145,7 @@ tQuery.register('createAugmentedJoystick', function(opts){
 	return texture;
 });
 
+/**
+ * equal to hasUserMedia
+*/
+tQuery.AugmentedJoystick.hasUserMedia	= navigator.webkitGetUserMedia ? true : false;
