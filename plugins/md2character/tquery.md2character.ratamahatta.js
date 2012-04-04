0d6fad120fdfc02b126811c662041776c50ec9a5
Jerome Etienne <jerome.etienne@gmail.com>
2012-04-04 15:45:10 +0200

    load all the weapon plugins/md2character

--- plugins/md2character/tquery.md2character.ratamahatta.js
+++ plugins/md2character/tquery.md2character.ratamahatta.js
@@ -26,17 +26,17 @@ tQuery.register('RatamahattaMD2Character', function(opts){
 		//skins	: [ "ratamahatta.png"],
 		skins	: [ "ratamahatta.png", "ctf_b.png", "ctf_r.png", "dead.png", "gearwhore.png" ],
 		weapons	: [
-			//[ "weapon.js", "weapon.png" ],
-			//[ "w_bfg.js", "w_bfg.png" ],
-			//[ "w_blaster.js", "w_blaster.png" ],
-			//[ "w_chaingun.js", "w_chaingun.png" ],
-			//[ "w_glauncher.js", "w_glauncher.png" ],
-			//[ "w_hyperblaster.js", "w_hyperblaster.png" ],
-			//[ "w_machinegun.js", "w_machinegun.png" ],
-			//[ "w_railgun.js", "w_railgun.png" ],
-			//[ "w_rlauncher.js", "w_rlauncher.png" ],
-			//[ "w_shotgun.js", "w_shotgun.png" ],
-			//[ "w_sshotgun.js", "w_sshotgun.png" ]
+			[ "weapon.js", "weapon.png" ],
+			[ "w_bfg.js", "w_bfg.png" ],
+			[ "w_blaster.js", "w_blaster.png" ],
+			[ "w_chaingun.js", "w_chaingun.png" ],
+			[ "w_glauncher.js", "w_glauncher.png" ],
+			[ "w_hyperblaster.js", "w_hyperblaster.png" ],
+			[ "w_machinegun.js", "w_machinegun.png" ],
+			[ "w_railgun.js", "w_railgun.png" ],
+			[ "w_rlauncher.js", "w_rlauncher.png" ],
+			[ "w_shotgun.js", "w_shotgun.png" ],
+			[ "w_sshotgun.js", "w_sshotgun.png" ]
 		]
 	});
 });
