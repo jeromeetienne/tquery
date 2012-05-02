96b9c998cfa89a1026d6a8bddf6aeb1b49bd0fb6
Jerome Etienne <jerome.etienne@gmail.com>
2012-05-03 01:09:31 +0200

    more work on docco doc in plugins/md2character

--- plugins/md2character/examples/index.js
+++ plugins/md2character/examples/index.js
@@ -4,7 +4,8 @@ var world	= tQuery.createWorld().boilerplate().start();
 // Change renderer clear color.
 world.renderer().setClearColorHex( 0x000000, world.renderer().getClearAlpha() );
 
-// ## Lights 
+// ## The Lights 
+
 // Add a ambient light and 2 directional lights
 tQuery.createAmbientLight().addTo(world).color(0x444444);
 tQuery.createDirectionalLight().addTo(world).position(-1,1,1).color(0xFF88BB).intensity(3);
@@ -13,29 +14,47 @@ tQuery.createDirectionalLight().addTo(world).position( 1,1,-1).color(0x4444FF).i
 // add a fog. This is always a nice trick to hide the "end of the world"
 world.addFogExp2({density : 0.1});
 
-// ### Columns
-// initialize a material for all the columns
+// # The Columns
+
+// ### initialize a material
+
+// We will apply it to all the columns. It is [lambert lighting](http://en.wikipedia.org/wiki/Lambertian_reflectance)
+// the ```ambient``` is the color which gonna be combined with be combined with the ambient
+// light we initialized on top. ```color``` will be combined with the
+// directional lights. and ```map``` gives the texture to use.
 var material	= new THREE.MeshLambertMaterial({
 	ambient	: 0xFFFFFF,
 	color	: 0xFFAAAA,
 	map	: THREE.ImageUtils.loadTexture('../../assets/images/water.jpg')
 });
-// build 15 columns
+
+// ### Build 15 Columns
+
+// loop over each column
 for(var i = 0; i < 15; i++ ){
-	// Create the cylinder
+	// Create the cylinder. We pass some parameters to the contructor to setup
+	// the size we see fit, and we add the material we want to apply on the
+	// cylinder. Then we use ```.addTo()``` to add our object to
+	// our ```tQuery.World```.
 	var column	= tQuery.createCylinder(0.2,0.2,2, material).addTo(world);
-	// Change the size of the default cylinder
+	// Change the position of the column. We translate the column to build
+	// a kind of alley. Thus the character will be able to run inside it :)
 	column.translateX(i%2 ? +1 : -1).translateY(1).translateZ(15/2 + -1*i);
 }
 
-// ### Add a ground
-// add a checkboard, 1 tile per 1 units
+// # The Ground
+
+// We create a large checkerboard with ```tquery.checkerboard.js``` plugin.
+// We scale the checkerboard to 100 per 100 units in the 3D world. Thus it is
+// quite large and disappears into the fog. It gives the cheap impression of
+// an infinite checkerboard.
 tQuery.createCheckerboard({
 	segmentsW	: 100,	// number of segment in width
 	segmentsH	: 100	// number of segment in Height
 }).addTo(world).scaleBy(100);
 
-// ## Character
+// # The Character 
+
 // Create a character, attached it to the world and hook the keyboard
 character	= new tQuery.RatamahattaMD2Character().attach(world).hookKeyboard();
 // Bind the "loaded" event, just to display the name of each animations
@@ -44,13 +63,17 @@ character.bind("loaded", function(){
 });
 
 // set the cameraControls	
-world.setCameraControls(new tQuery.MD2Character.CameraControls(character))
+world.setCameraControls(new tQuery.MD2Character.CameraControls(character));
 
-// ### Change the skin by keyboard
-//
+// ## Change the Skin by Keyboard
+
+// hook a function in the rendering loop. This function will be executed everytime
+// the scene is rendered. Within this function, we will use ```tQuery.Keyboard```
+// plugins to test the keyboard. if the key ```s``` is pressed, then use ```character.setSkin()```
+// to change the skin of the character
 world.loop().hook(function(){
-	// get keyboard instance
-	var keyboard	= tQuery.keyboard();
-	// if the key 's' is pressed, change the skin
-	if( keyboard.pressed("s") )	character.setSkin(Math.floor(Math.random()*5));
-})
+	var keyboard	= tQuery.keyboard();	// get keyboard instance
+	if( keyboard.pressed("s") ){		// if the key 's' is pressed, change the skin
+		character.setSkin(Math.floor(Math.random()*5));
+	}
+});
