29b48b29f9d1c50ff7b0b258ab4bdd2f134cf347
Jerome Etienne <jerome.etienne@gmail.com>
2012-05-25 17:35:01 +0200

    more work on material. almost looking good plugins/materials

---
+++ plugins/materials/tquery.meshbasicmaterial.js
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.Mesh.register('setBasicMaterial', function(opts){
+	var material	= tQuery.createMeshBasicMaterial(opts);
+	this.material( material.get(0) );
+	return material.back(this);
+})
+
+tQuery.register('createMeshBasicMaterial', function(opts){
+	var tMaterial	= new THREE.MeshBasicMaterial(opts);
+	var material	= new tQuery.MeshBasicMaterial(tMaterial);
+	return material;
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Handle directional light
+ *
+ * @class include THREE.BasicMaterial. It inherit from {@link tQuery.Material}
+ * 
+ * @borrows tQuery.Node#get as this.get
+ * @borrows tQuery.Node#each as this.each
+ * @borrows tQuery.Node#back as this.back
+ *
+ * @param {THREE.BasicMaterial} element an instance or array of instance
+*/
+tQuery.MeshBasicMaterial	= function(elements)
+{
+	// call parent ctor
+	tQuery.MeshBasicMaterial.parent.constructor.call(this, elements)
+
+	// sanity check - all items MUST be THREE.Material
+	this._lists.forEach(function(item){ console.assert(item instanceof THREE.MeshBasicMaterial); });
+};
+
+/**
+ * inherit from tQuery.Material
+*/
+tQuery.inherit(tQuery.MeshBasicMaterial, tQuery.Material);
+
+/**
+ * define all acceptable attributes for this class
+*/
+tQuery.mixinAttributes(tQuery.MeshBasicMaterial, {
+	color		: tQuery.convert.toThreeColor,
+	ambient		: tQuery.convert.toThreeColor,
+	wireframe	: tQuery.convert.toBool
+});
+
+
---
+++ plugins/materials/tquery.meshphongmaterial.js
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.Mesh.register('setPhongMaterial', function(opts){
+	var material	= tQuery.createMeshPhongMaterial(opts);
+	this.material( material.get(0) );
+	return material.back(this);
+})
+
+tQuery.register('createMeshPhongMaterial', function(opts){
+	var tMaterial	= new THREE.MeshPhongMaterial(opts);
+	var material	= new tQuery.MeshPhongMaterial(tMaterial);
+	return material;
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+//										//
+//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Handle directional light
+ *
+ * @class include THREE.PhongMaterial. It inherit from {@link tQuery.Material}
+ * 
+ * @borrows tQuery.Node#get as this.get
+ * @borrows tQuery.Node#each as this.each
+ * @borrows tQuery.Node#back as this.back
+ *
+ * @param {THREE.PhongMaterial} element an instance or array of instance
+*/
+tQuery.MeshPhongMaterial	= function(elements)
+{
+	// call parent ctor
+	tQuery.MeshPhongMaterial.parent.constructor.call(this, elements)
+
+	// sanity check - all items MUST be THREE.Material
+	this._lists.forEach(function(item){ console.assert(item instanceof THREE.MeshPhongMaterial); });
+};
+
+/**
+ * inherit from tQuery.Material
+*/
+tQuery.inherit(tQuery.MeshPhongMaterial, tQuery.Material);
+
+/**
+ * define all acceptable attributes for this class
+*/
+tQuery.mixinAttributes(tQuery.MeshPhongMaterial, {
+	color		: tQuery.convert.toThreeColor,
+	ambient		: tQuery.convert.toThreeColor,
+	specular	: tQuery.convert.toThreeColor,
+	shininess	: tQuery.convert.toNumber
+});
+
+
