1d0ae3ba578975a93bd2c3e2beaeb70f454527a6
Jerome Etienne <jerome.etienne@gmail.com>
2012-05-26 16:13:58 +0200

    admin commit

--- build/tquery-bundle.js
+++ build/tquery-bundle.js
@@ -2199,6 +2199,17 @@ tQuery.convert.toBool	= function(value){
 	}
 	return undefined;	// never reached - just to workaround linter complaint
 };
+
+tQuery.convert.toTexture	= function(value){
+	if( arguments.length === 1 && value instanceof THREE.Texture ){
+		return value;
+	}else if( arguments.length === 1 && typeof(value) === 'string' ){
+		return THREE.ImageUtils.loadTexture(value);
+	}else{
+		console.assert(false, "invalid parameter");
+	}
+	return undefined;	// never reached - just to workaround linter complaint
+};
 /**
  * implementation of the tQuery.Node
  *
