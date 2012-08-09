0d8b1dd2b89c884d50c742d367bdd0e11d57d9a1
Jerome Etienne <jerome.etienne@gmail.com>
2012-08-09 23:46:51 +0200

    changed pick array method to support orthographic camera - thanks @leconcepteur

--- plugins/domevent/threex.domevent.js
+++ plugins/domevent/threex.domevent.js
@@ -248,10 +248,8 @@ THREEx.DomEvent.prototype._bound	= function(eventName, object3d)
 THREEx.DomEvent.prototype._onMove	= function(mouseX, mouseY, origDomEvent)
 {
 	var vector	= new THREE.Vector3( mouseX, mouseY, 0.5 );
-	this._projector.unprojectVector( vector, this._camera );
-
-	var ray		= new THREE.Ray( this._camera.position, vector.subSelf( this._camera.position ).normalize() );
-	var intersects = ray.intersectObjects( this._boundObjs );
+	var ray         = this._projector.pickingRay( vector, this._camera );
+	var intersects  = ray.intersectObjects( this._boundObjs );
 	
 	var oldSelected	= this._selected;
 	
@@ -294,12 +292,8 @@ THREEx.DomEvent.prototype._onMove	= function(mouseX, mouseY, origDomEvent)
 THREEx.DomEvent.prototype._onEvent	= function(eventName, mouseX, mouseY, origDomEvent)
 {
 	var vector	= new THREE.Vector3( mouseX, mouseY, 1 );
-// TODO to change... see @leconcepteur bug on github. apparently this tech is only for perspectiveCamera
-	this._projector.unprojectVector( vector, this._camera );
-
-	vector.subSelf( this._camera.position ).normalize()
-	var ray		= new THREE.Ray( this._camera.position, vector );
-	var intersects	= ray.intersectObjects( this._boundObjs );
+	var ray         = this._projector.pickingRay( vector, this._camera );
+	var intersects  = ray.intersectObjects( this._boundObjs );
 
 	// if there are no intersections, return now
 	if( intersects.length === 0 )	return;
