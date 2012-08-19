1f6204bd65785c8164ad8c65c919f9f1b88286fe
Jerome Etienne <jerome.etienne@gmail.com>
2012-08-19 11:58:45 +0200

    bug fix - typo

--- js/tquery.core.js
+++ js/tquery.core.js
@@ -171,7 +171,7 @@ tQuery.extend = function(obj, base, deep){
 	var extendFn	= deep ? deepExtend : shallowExtend;
 	var result	= {};
 	base	&& extendFn(result, base);
-	obj	&& extendFn(result, base);
+	obj	&& extendFn(result, obj);
 	return result;
 	
 	function shallowExtend(dst, src){
