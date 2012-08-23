517ae48c8c244e5b7d68098177658a1bbe79a547
Jerome Etienne <jerome.etienne@gmail.com>
2012-08-23 22:04:19 +0200

    cleanup

--- plugins/requirejs/head.confrequire.js
+++ plugins/requirejs/head.confrequire.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview this is the initial requirejs.config(). the one which establish the 
+ * path of each parts of the project.
+ * TODO is it generic enought ? could it be using baseURL ?
+*/
 requirejs.config({
 	paths	: {
 		"build"		: "../../../build",
