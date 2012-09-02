c7fe779bed8d5724f170e4b1751d4ee517d96480
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-02 21:32:18 +0200

    added Object3D.detach

--- js/tquery.object3d.js
+++ js/tquery.object3d.js
@@ -147,6 +147,20 @@ tQuery.Object3D.prototype.removeFrom	= function(target)
 	return this;
 }
 
+/**
+ * remove an element from the parent to which it is attached
+ * 
+ * @returns {tQuery.Object3D} chained API
+*/
+tQuery.Object3D.prototype.detach	= function()
+{
+	this.each(function(object3D){
+		if( !object3D.parent )	return;
+		object3D.parent.remove(object3D)
+	}.bind(this));
+	return this;
+}
+
 //////////////////////////////////////////////////////////////////////////////////
 //			addTo/removeFrom tQuery.World/tQuery.Object3d		//
 //////////////////////////////////////////////////////////////////////////////////
@@ -189,6 +203,7 @@ tQuery.Object3D.prototype.remove	= function(tqObject3d)
 	}.bind(this));
 	return this;
 }
+
 
 //////////////////////////////////////////////////////////////////////////////////
 //		Handle dom attribute						//
