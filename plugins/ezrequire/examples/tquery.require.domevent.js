327020b36722f85246b1b56c97834295fa7eaf4d
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-04 02:15:18 +0200

    removed obsolete file from previous require.js attempt

--- plugins/ezrequire/examples/tquery.require.domevent.js
+++
@@ -1,6 +0,0 @@
-tQuery.define(["plugins/domevent/threex.domevent.js",
-	       "plugins/domevent/tquery.object3d.domevent.js"], function(){
-	console.log("domevent loaded")
-	//alert("all loaded")
-	//debugger;
-})
