c994c9e025b2bb28729768a8eee5a65c3d983050
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-06 08:40:13 +0200

    more comment

--- plugins/light/tquery.light.shadow.js
+++ plugins/light/tquery.light.shadow.js
@@ -1,7 +1,13 @@
+/**
+ * little helper to set the width and height of the shadowMap 
+*/
 tQuery.DirectionalLight.register('shadowMap', function(width, height){
 	return this.shadowMapWidth(width).shadowMapHeight(height);
 });
 
+/**
+ * little helper to set all shadowCamera params
+*/
 tQuery.DirectionalLight.register('shadowCamera', function(right, left, top, bottom, near, far){
 	if( near !== undefined && far !== undefined ){
 		this.shadowCameraNear(near).shadowCameraFar(far);
