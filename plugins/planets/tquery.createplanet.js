155201f4f3e2b47866bce02967cdc842d5793e36
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-12 18:19:44 +0200

    better examples in tquery.planets

--- plugins/planets/tquery.createplanet.js
+++ plugins/planets/tquery.createplanet.js
@@ -10,20 +10,23 @@ tQuery.register('createPlanet', function(opts){
 	var baseUrl	= tQuery.createPlanet.baseUrl;
 	if( opts.type === 'moon' ){
 		var url		= baseUrl + 'images/moon_1024.jpg';	
-		var object	= tQuery.createSphere();
+		var object	= tQuery.createSphere()
+					.addClass('planet')
 		object.material(new THREE.MeshBasicMaterial({
 			map	: THREE.ImageUtils.loadTexture(url)
 		}));
 	}else if( opts.type === 'earth' ){
 		var object	= tQuery.createObject3D()
 
-		var earth	= tQuery.createSphere(0.5, 100, 50).addTo(object);
+		var earth	= tQuery.createSphere(0.5, 100, 50).addTo(object)
+					.addClass('planet')
 		var url		= baseUrl + 'images/earth_atmos_2048.jpg';
 		earth.material(new THREE.MeshBasicMaterial({
 			map	: THREE.ImageUtils.loadTexture(url)
 		}));
 
-		var cloud	= tQuery.createSphere(0.52, 100, 50).addTo(object);
+		var cloud	= tQuery.createSphere(0.52, 100, 50).addTo(object)
+					.addClass('cloud')
 		var url		= baseUrl + 'images/earth_clouds_1024.png';
 		cloud.material(new THREE.MeshBasicMaterial({
 			map		: THREE.ImageUtils.loadTexture(url),
@@ -32,19 +35,18 @@ tQuery.register('createPlanet', function(opts){
 	}else if( opts.type === 'earthNormal' ){
 		var object	= tQuery.createObject3D()
 
-		var planetTexture	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_atmos_2048.jpg" );
-		var cloudsTexture	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_clouds_1024.png" );
-		var normalTexture	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_normal_2048.jpg" );
-		var specularTexture	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_specular_2048.jpg" );
+		var planetTex	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_atmos_2048.jpg" );
+		var normalTex	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_normal_2048.jpg" );
+		var specularTex	= THREE.ImageUtils.loadTexture( baseUrl+"images/earth_specular_2048.jpg" );
 
 		var shader	= THREE.ShaderUtils.lib[ "normal" ];
 		var uniforms	= THREE.UniformsUtils.clone( shader.uniforms );
 
-		uniforms[ "tNormal" ].texture	= normalTexture;
+		uniforms[ "tNormal" ].texture	= normalTex;
 		uniforms[ "uNormalScale" ].value= 0.85;
 
-		uniforms[ "tDiffuse" ].texture	= planetTexture;
-		uniforms[ "tSpecular" ].texture = specularTexture;
+		uniforms[ "tDiffuse" ].texture	= planetTex;
+		uniforms[ "tSpecular" ].texture = specularTex;
 
 		uniforms[ "enableAO" ].value		= false;
 		uniforms[ "enableDiffuse" ].value	= true;
@@ -70,7 +72,8 @@ tQuery.register('createPlanet', function(opts){
 		var geometry	= new THREE.SphereGeometry(0.5, 30, 15 );
 		geometry.computeTangents();
 
-		tQuery(geometry, materialNormalMap).addTo(object);
+		tQuery(geometry, materialNormalMap).addTo(object)
+			.addClass('planet')
 
 		// clouds
 		var url		= baseUrl + 'images/earth_clouds_1024.png';
@@ -82,7 +85,8 @@ tQuery.register('createPlanet', function(opts){
 		});
 
 		tQuery( geometry, materialClouds ).addTo(object)
-			.scaleBy(1.005)
+			.addClass('cloud')
+			.scaleBy(1.01)
 	}else	console.assert(false, 'unknown opts.type: '+opts.type)
 
 	return object;
