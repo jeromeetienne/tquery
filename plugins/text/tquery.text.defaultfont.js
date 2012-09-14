64c6817922344fdb9e9008b6eba72663ec12fa0a
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-14 18:38:06 +0200

    removed obsolere requirejs files

--- plugins/text/tquery.text.defaultfont.js
+++
@@ -1,6 +0,0 @@
-define([
-	'plugins/text/fonts/droid/droid_serif_bold.typeface',
-	], function(){
-	
-	// not doing much
-});
--- plugins/text/tquery.text.js
+++ plugins/text/tquery.text.js
@@ -4,28 +4,28 @@
 tQuery.register('createText', function(text, options){
 	// handle parameters
 	options	= tQuery.extend(options, {
+		font		: "droid serif",
+		weight		: "bold",
 		size		: 1,
 		height		: 0.4,
-		weight		: "bold",
-		font		: "droid serif",
 	});
-	// create the geometry
-	var geometry	= new THREE.TextGeometry(text, options);
+	// create the tGeometry
+	var tGeometry	= new THREE.TextGeometry(text, options);
 
-	// center the geometry
+	// center the tGeometry
 	// - THREE.TextGeometry isnt centered for unknown reasons. all other geometries are centered
-	geometry.computeBoundingBox();
+	tGeometry.computeBoundingBox();
 	var center	= new THREE.Vector3();
-	center.x	= ( geometry.boundingBox.max.x - geometry.boundingBox.min.x ) / 2;
-	center.y	= ( geometry.boundingBox.max.y - geometry.boundingBox.min.y ) / 2;
-	center.z	= ( geometry.boundingBox.max.z - geometry.boundingBox.min.z ) / 2;
-	geometry.vertices.forEach(function(vertex){
+	center.x	= ( tGeometry.boundingBox.max.x - tGeometry.boundingBox.min.x ) / 2;
+	center.y	= ( tGeometry.boundingBox.max.y - tGeometry.boundingBox.min.y ) / 2;
+	center.z	= ( tGeometry.boundingBox.max.z - tGeometry.boundingBox.min.z ) / 2;
+	tGeometry.vertices.forEach(function(vertex){
 		vertex.subSelf(center);
 	});
 	
 	// create a mesh with it
-	var material	= new THREE.MeshNormalMaterial();
-	var mesh	= new THREE.Mesh( geometry, material );
+	var tMaterial	= new THREE.MeshNormalMaterial();
+	var mesh	= new THREE.Mesh( tGeometry, tMaterial );
 	// return a tQuery object
 	return tQuery(mesh);
 });
