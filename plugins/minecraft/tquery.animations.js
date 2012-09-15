c65644618eca7daff9c632e52ccf833b7d0b65a9
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-15 12:53:10 +0200

    added .animationName() getter

--- plugins/minecraft/tquery.animations.js
+++ plugins/minecraft/tquery.animations.js
@@ -17,6 +17,7 @@ tQuery.registerStatic('createAnimations', function(){
 tQuery.registerStatic('Animations', function(){
 	this._animations	= {};
 	this._currentAnim	= null;
+	this._animationName	= null;
 });
 
 /**
@@ -65,8 +66,12 @@ tQuery.Animations.prototype.names	= function(){
  * @param {string} animationName the name of the animation
 */
 tQuery.Animations.prototype.start	= function(animationName){
+	// if this animation is already the current one, do nothing
+	if( this._animationName === animationName )	return this;
+	// stop current animation
 	if( this.isRunning() )	this.stop();
 	console.assert( this._animations[animationName] !== undefined, "unknown animation name: "+animationName)
+	this._animationName	= animationName;
 	this._currentAnim	= this._animations[animationName];
 	this._currentAnim.start();
 	return this;	// for chained API
@@ -81,11 +86,16 @@ tQuery.Animations.prototype.isRunning	= function(){
 	return this._currentAnim ? true : false;
 }
 
+tQuery.Animations.prototype.animationName	= function(){
+	return this._animationName;
+}
+
 /**
  * Stop the running animation if any
 */
 tQuery.Animations.prototype.stop	= function(){
 	this._currentAnim	&& this._currentAnim.destroy();
 	this._currentAnim	= null;
+	this._animationName	= null;
 	return this;	// for chained API
 }
