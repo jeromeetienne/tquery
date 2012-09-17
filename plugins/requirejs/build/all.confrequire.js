ae458ae926c29b0e0828d77309ce0f4a04f8bdd0
Jerome Etienne <jerome.etienne@gmail.com>
2012-09-17 08:41:56 +0200

    more work on workshop

--- plugins/requirejs/build/all.confrequire.js
+++ plugins/requirejs/build/all.confrequire.js
@@ -95,9 +95,13 @@ requirejs.config({
 			"plugins/md2character/tquery.md2character.ratamahatta"
 		],
 		"plugins/requirejs/confrequire/minecraft.initrequire": [
+			"plugins/minecraft/tquery.midikeytween",
 			"plugins/minecraft/tquery.minecraftchar",
+			"plugins/minecraft/tquery.minecraftchar.keyboard2",
+			"plugins/minecraft/tquery.camerafpscontrols",
 			"plugins/minecraft/tquery.animation",
 			"plugins/minecraft/tquery.animations",
+			"plugins/minecraft/tquery.spritesheet",
 			"plugins/minecraft/tquery.minecraftcharanimations",
 			"plugins/minecraft/tquery.minecraftcharheadanimations"
 		],
--- plugins/requirejs/confrequire/minecraft.confrequire.js
+++ plugins/requirejs/confrequire/minecraft.confrequire.js
@@ -6,9 +6,13 @@ requirejs.config({
 	},
 	shim	: {
 		'plugins/requirejs/confrequire/minecraft.initrequire'	: [
+			'plugins/minecraft/tquery.midikeytween',
 			'plugins/minecraft/tquery.minecraftchar',
+			'plugins/minecraft/tquery.minecraftchar.keyboard2',
+			'plugins/minecraft/tquery.camerafpscontrols',
 			'plugins/minecraft/tquery.animation',
 			'plugins/minecraft/tquery.animations',
+			'plugins/minecraft/tquery.spritesheet',
 			'plugins/minecraft/tquery.minecraftcharanimations',
 			'plugins/minecraft/tquery.minecraftcharheadanimations',
 		]
