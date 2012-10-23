f79aa03471eb21b8e322ff208e5e3914920d689d
Jerome Etienne <jerome.etienne@gmail.com>
2012-10-23 15:43:48 +0200

    more work

--- plugins/requirejs/head.confrequire.js
+++ plugins/requirejs/head.confrequire.js
@@ -7,7 +7,7 @@ requirejs.config({
 	paths	: {
 		"build"		: "../../../build",
 		"plugins"	: "../../../plugins",
-		'threex'	: '../../../vendor/threex',
-		'three.js'	: '../../../vendor/three.js',
+		"threex"	: '../../../vendor/threex',
+		"three.js"	: '../../../vendor/three.js',
 	},
 });
