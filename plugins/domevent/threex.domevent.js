5a5fc49fe49fe2b46763664af1fd2b74f744e7cc
Jerome Etienne <jerome.etienne@gmail.com>
2013-01-04 11:52:55 +0100

    removed bug on the onContextmenu

--- plugins/domevent/threex.domevent.js
+++ plugins/domevent/threex.domevent.js
@@ -86,7 +86,7 @@ THREEx.DomEvent	= function(camera, domElement)
 	this._$onTouchMove	= function(){ _this._onTouchMove.apply(_this, arguments);	};
 	this._$onTouchStart	= function(){ _this._onTouchStart.apply(_this, arguments);	};
 	this._$onTouchEnd	= function(){ _this._onTouchEnd.apply(_this, arguments);	};
-	this._$onContextmenu= function(){ _this.onContextmenu.apply(_this, arguments);	};
+	this._$onContextmenu	= function(){ _this._onContextmenu.apply(_this, arguments);	};
 	this._domElement.addEventListener( 'click'	, this._$onClick	, false );
 	this._domElement.addEventListener( 'dblclick'	, this._$onDblClick	, false );
 	this._domElement.addEventListener( 'mousemove'	, this._$onMouseMove	, false );
