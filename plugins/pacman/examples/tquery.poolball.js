08f8e40c745c9cbeeecabb87e729fe8748f691cd
Jerome Etienne <jerome.etienne@gmail.com>
2013-02-06 21:43:48 +0100

    more on tquery.poolball.js

--- plugins/pacman/examples/tquery.poolball.js
+++ plugins/pacman/examples/tquery.poolball.js
@@ -60,6 +60,7 @@ tQuery.createPoolBall.draw	= function(canvas, textData, stripped, fillStyle){
 	var ctx		= canvas.getContext( '2d' );
 	var w		= canvas.width;
 	var h		= canvas.height;
+	var canvasX	= tQuery.createPoolBall.canvasX;
 	
 	// base color is white
 	ctx.save();
@@ -82,7 +83,7 @@ tQuery.createPoolBall.draw	= function(canvas, textData, stripped, fillStyle){
 		ctx.fillStyle	= "#FFFFFF";
 		var radiusW	= 0.7 * w/4;
 		var radiusH	= 1.2 * h/4;
-		ctx.fillEllipse( -radiusW/2, -radiusH/2, radiusW, radiusH);
+		canvasX.fillEllipse(ctx, -radiusW/2, -radiusH/2, radiusW, radiusH);
 		ctx.restore();
 
 		// draw text data
@@ -110,4 +111,47 @@ tQuery.createPoolBall.ballTexture	= function( textData, stripped, fillStyle, can
 	tQuery.createPoolBall.draw(canvas, textData, stripped, fillStyle);
 	// return the texture
 	return texture;
+};
+
+tQuery.createPoolBall.canvasX	= {
+	// Andrea Giammarchi - Mit Style License
+	// Circle methods
+	circle		: function(ctx, aX, aY, aDiameter){
+		this.ellipse(ctx, aX, aY, aDiameter, aDiameter);
+	},
+	fillCircle	: function(ctx, aX, aY, aDiameter){
+		ctx.beginPath();
+		this.circle(ctx, aX, aY, aDiameter);
+		ctx.fill();
+	},
+	strokeCircle	: function(ctx, aX, aY, aDiameter){
+		ctx.beginPath();
+		this.circle(ctx, aX, aY, aDiameter);
+		ctx.stroke();
+	},
+	// Ellipse methods
+	ellipse		: function(ctx, aX, aY, aWidth, aHeight){
+		var hB = (aWidth / 2) * .5522848,
+		vB = (aHeight / 2) * .5522848,
+		eX = aX + aWidth,
+		eY = aY + aHeight,
+		mX = aX + aWidth / 2,
+		mY = aY + aHeight / 2;
+		ctx.moveTo(aX, mY);
+		ctx.bezierCurveTo(aX, mY - vB, mX - hB, aY, mX, aY);
+		ctx.bezierCurveTo(mX + hB, aY, eX, mY - vB, eX, mY);
+		ctx.bezierCurveTo(eX, mY + vB, mX + hB, eY, mX, eY);
+		ctx.bezierCurveTo(mX - hB, eY, aX, mY + vB, aX, mY);
+		ctx.closePath();
+	},
+	fillEllipse	: function(ctx, aX, aY, aWidth, aHeight){
+		ctx.beginPath();
+		this.ellipse(ctx, aX, aY, aWidth, aHeight);
+		ctx.fill();
+	},
+	strokeEllipse:function(ctx, aX, aY, aWidth, aHeight){
+		ctx.beginPath();
+		this.ellipse(ctx, aX, aY, aWidth, aHeight);
+		ctx.stroke();
+	}
 };
