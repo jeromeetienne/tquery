19d141ee3c9af1066574145bfc1c728629c7e914
Jerome Etienne <jerome.etienne@gmail.com>
2013-03-03 09:05:30 +0100

    added microcache in tquery.core

--- js/tquery.core.js
+++ js/tquery.core.js
@@ -387,3 +387,24 @@ tQuery.MicroeventMixin	= function(destObj){
 	}
 };
 
+/**
+ * https://github.com/jeromeetienne/MicroCache.js
+*/
+tQuery.MicroCache	= function(){
+	var _values	= {};
+	return {
+		get	: function(key){ return _values[key];	},
+		contains: function(key){ return key in _values;	},
+		remove	: function(key){ delete _values[key];	},
+		set	: function(key, value){	_values[key] = value;},
+		values	: function(){ return _values;	},
+		getSet	: function(key, value){
+			if( !this.contains(key) ){
+				this.set(key, typeof value == 'function' ? value() : value )
+			}
+			return this.get(key);
+		}
+	}
+}
+
+
