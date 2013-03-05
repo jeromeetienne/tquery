fd90dc775b2d4e7cf1bb62126a6ad754d30e392c
Jerome Etienne <jerome.etienne@gmail.com>
2013-03-06 00:18:32 +0100

    less intrusive autoconfig - fix #260

--- plugins/requirejs/footer.js
+++ plugins/requirejs/footer.js
@@ -1,9 +1,14 @@
 (function(){
+	// get the script dom element which included the library
 	var scripts	= document.getElementsByTagName('script');
-	var element	= scripts[scripts.length-1];
-	var baseUrl	= element.getAttribute('data-baseURL');
-	console.log('baseUrl', baseUrl, element);
-	if( baseUrl === null )	return;
+	var scriptEl	= scripts[scripts.length-1];
+	var src		= scriptEl.src;
+	var suffix	= '/build/tquery-bundle-require.js';
+	// if the element src DOES NOT endup with suffix, do nothing
+	if(src.indexOf(suffix, src.length - suffix.length) !== -1)	return;
+	// get the baseURL
+	var baseURL	= src.substr(0, src.length - suffix.length)
+	// configure require.js using this baseUrl
 	requirejs.config({
 		paths	: {
 			"build"		: baseUrl+'/build',
@@ -12,4 +17,4 @@
 			"three.js"	: baseUrl+'/vendor/three.js',
 		},
 	});
-})()
+})();
