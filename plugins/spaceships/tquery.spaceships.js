803bcdde35586110512e489bee78ef186248c9b7
Jerome Etienne <jerome.etienne@gmail.com>
2013-03-08 12:01:47 +0100

    more on spaceships

--- plugins/spaceships/tquery.spaceships.js
+++ plugins/spaceships/tquery.spaceships.js
@@ -14,21 +14,137 @@ tQuery.pluginsStaticOn(tQuery.SpaceShips);
 //		comment								//
 //////////////////////////////////////////////////////////////////////////////////
 
-tQuery.registerStatic('loadSpaceShip3', function(){
+tQuery.registerStatic('loadShuttle01', function(){
+	var loader	= new THREE.OBJMTLLoader();
+	loader.addEventListener('load', function( event ){
+		tQuery('mesh', event.content)
+			.geometry()
+				.scaleBy(1/400)
+				.computeAll()
+				.back()
+	});
+	var baseUrl	= tQuery.SpaceShips.baseUrl;
+	var objUrl	= baseUrl + 'models/Shuttle01/Shuttle01.obj';
+	var mtlUrl	= baseUrl + 'models/Shuttle01/Shuttle01.mtl';
+	loader.load(objUrl, mtlUrl);
+	return loader;
+});
+
+tQuery.registerStatic('createShuttle01', function(){
+	var container	= tQuery.createObject3D()
+	var loader	= tQuery.loadShuttle01()
+	loader.addEventListener('load', function(event){
+		tQuery(event.content).addTo(container)
+	})
+	return container;
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+//		comment								//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.registerStatic('loadShuttle02', function(){
+	var loader	= new THREE.OBJMTLLoader();
+	loader.addEventListener('load', function( event ){
+		tQuery('mesh', event.content)
+			.geometry()
+				.scaleBy(1/400)
+				.computeAll()
+				.back()
+	});
+	var baseUrl	= tQuery.SpaceShips.baseUrl;
+	var objUrl	= baseUrl + 'models/Shuttle02/Shuttle02.obj';
+	var mtlUrl	= baseUrl + 'models/Shuttle02/Shuttle02.mtl';
+	loader.load(objUrl, mtlUrl);
+	return loader;
+});
+
+tQuery.registerStatic('createShuttle02', function(){
+	var container	= tQuery.createObject3D()
+	var loader	= tQuery.loadShuttle02()
+	loader.addEventListener('load', function(event){
+		tQuery(event.content).addTo(container)
+	})
+	return container;
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+//		comment								//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.registerStatic('loadSpaceFighter01', function(){
+	var loader	= new THREE.OBJMTLLoader();
+	loader.addEventListener('load', function( event ){
+		tQuery('mesh', event.content)
+			.geometry()
+				.scaleBy(1/300)
+				.computeAll()
+				.back()
+	});
+	var baseUrl	= tQuery.SpaceShips.baseUrl;
+	var objUrl	= baseUrl + 'models/SpaceFighter01/SpaceFighter01.obj';
+	var mtlUrl	= baseUrl + 'models/SpaceFighter01/SpaceFighter01.mtl';
+	loader.load(objUrl, mtlUrl);
+	return loader;
+});
+
+tQuery.registerStatic('createSpaceFighter01', function(){
+	var container	= tQuery.createObject3D()
+	var loader	= tQuery.loadSpaceFighter01()
+	loader.addEventListener('load', function(event){
+		tQuery(event.content).addTo(container)
+	})
+	return container;
+});
+	
+//////////////////////////////////////////////////////////////////////////////////
+//		comment								//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.registerStatic('loadSpaceFighter02', function(){
+	var loader	= new THREE.OBJMTLLoader();
+	loader.addEventListener('load', function( event ){
+		tQuery('mesh', event.content)
+			.geometry()
+				.scaleBy(1/200)
+				.computeAll()
+				.back()
+	});
+	var baseUrl	= tQuery.SpaceShips.baseUrl;
+	var objUrl	= baseUrl + 'models/SpaceFighter02/SpaceFighter02.obj';
+	var mtlUrl	= baseUrl + 'models/SpaceFighter02/SpaceFighter02.mtl';
+	loader.load(objUrl, mtlUrl);
+	return loader;
+});
+
+tQuery.registerStatic('createSpaceFighter02', function(){
+	var container	= tQuery.createObject3D()
+	var loader	= tQuery.loadSpaceFighter02()
+	loader.addEventListener('load', function(event){
+		tQuery(event.content).addTo(container)
+	})
+	return container;
+});	
+
+//////////////////////////////////////////////////////////////////////////////////
+//		comment								//
+//////////////////////////////////////////////////////////////////////////////////
+
+tQuery.registerStatic('loadSpaceFighter03', function(){
 	var loader	= new THREE.OBJMTLLoader();
 	loader.addEventListener('load', function( event ){
 		tQuery(event.content).scaleBy(1/10)
 	});
 	var baseUrl	= tQuery.SpaceShips.baseUrl;
-	var objUrl	= baseUrl + 'models/SpaceFighter03/untitled.obj';
-	var mtlUrl	= baseUrl + 'models/SpaceFighter03/untitled.mtl';
+	var objUrl	= baseUrl + 'models/SpaceFighter03/SpaceFighter03.obj';
+	var mtlUrl	= baseUrl + 'models/SpaceFighter03/SpaceFighter03.mtl';
 	loader.load(objUrl, mtlUrl);
 	return loader;
 });
 
-tQuery.registerStatic('createSpaceShip3', function(){
+tQuery.registerStatic('createSpaceFighter03', function(){
 	var container	= tQuery.createObject3D()
-	var loader	= tQuery.loadSpaceShip3()
+	var loader	= tQuery.loadSpaceFighter03()
 	loader.addEventListener('load', function(event){
 		tQuery(event.content).addTo(container)
 	})
--- plugins/spaceships/tquery.spaceships.shoot.js
+++ plugins/spaceships/tquery.spaceships.shoot.js
@@ -6,23 +6,25 @@ tQuery.SpaceShips.registerStatic('createShoot', function(){
 
 	// do the material	
 	var material	= new THREE.MeshBasicMaterial({
-		map		: texture,
-		blending	: THREE.AdditiveBlending,
 		color		: 0xffaacc,
+		map		: texture,
 		side		: THREE.DoubleSide,
+		blending	: THREE.AdditiveBlending,
 		depthWrite	: false,
 		transparent	: true
 	})
 
-	this._container	= tQuery.createObject3D()
+	var container	= tQuery.createObject3D()
 		.rotateY(Math.PI/2)
 		.scaleBy(1/2)
 	var nPlanes	= 4;
 	for(var i = 0; i < nPlanes; i++){
-		tQuery.createPlane().addTo(this._container)
+		tQuery.createPlane().addTo(container)
 			.material(material)
 			.rotateX(i*Math.PI/nPlanes)
 	}
+	
+	return container	
 		
 	function generateShootCanvas(){
 		// init canvas
@@ -35,9 +37,9 @@ tQuery.SpaceShips.registerStatic('createShoot', function(){
 			canvas.width/2, canvas.height /2, 0,
 			canvas.width/2, canvas.height /2, canvas.width /2
 		);		
-		gradient.addColorStop( 0  , 'rgba(255,255,255,0.7)' );
-		gradient.addColorStop( 0.5, 'rgba(192,192,192,0.5)' );
-		gradient.addColorStop( 0.8, 'rgba(128,128,128,0.3)' );
+		gradient.addColorStop( 0  , 'rgba(255,255,255,1)' );
+		gradient.addColorStop( 0.5, 'rgba(192,192,192,1)' );
+		gradient.addColorStop( 0.8, 'rgba(128,128,128,0.7)' );
 		gradient.addColorStop( 1  , 'rgba(0,0,0,0)' );
 
 		// fill the rectangle
@@ -46,6 +48,4 @@ tQuery.SpaceShips.registerStatic('createShoot', function(){
 		// return the just built canvas 
 		return canvas;	
 	};
-	
-	return this._container	
 });
